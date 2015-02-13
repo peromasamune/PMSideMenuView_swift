3bf621fc338ef5ac76041e7fc60cf8d2f9465dc1
t_inoue <t_inoue@yumemi.co.jp>
2015-02-13 18:08:14 +0900

    Added initial application

---
+++ PMSideMenuView_Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PMSideMenuView_Demo
+//
+//  Created by Taku Inoue on 2015/02/13.
+//  Copyright (c) 2015年 Taku Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        self.title = "Demo"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
